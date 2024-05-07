a5b6eae9dac5d6f1ce225ce7d463da4e74043d94
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-07 15:54:33 -0700

    Modified calculator.py; move calc messages to a dict, access by key

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -2,8 +2,23 @@
     Ask the user for two numbers.
     Ask the user for operation to perform: add, subtract, multiply or divide.
     Perform the calculation and display the results
-    Ask user if they wanto continue calculating: Y = start over, N =end program
+    Ask user if they wanto continue calculating: 
+        Y = start over, N =end program
+    
     '''
+# ORGANIZE CALC MESSAGES IN DICT
+messages = {
+'another_calc' : 'Would you like to do another calculation?'
+                ' Enter Y to continue OR any other key to stop.',
+'bye' : 'See you later!',
+'welcome' : 'Welcome to Calculator!',
+'num1' : 'Enter a number!',
+'num2' : 'Enter another number!',
+'num_error' : "Error: That isn't a valid number, expecting an integer!",
+'operation': 'What operation would you like to perform?:\n'
+            '   1) Addition 2) Subtraction 3) Multiplication 4) Division',
+'operation_error' : 'You must choose 1, 2, 3, or 4.'
+}
 
 def prompt(message):
     print(f"==> {message}")
@@ -16,39 +31,36 @@ def invalid_input(str_num):
     return False
 
 def calc_again():
-    prompt('''Would you like to do another calculation?
-        Enter Y to continue OR any other key to stop.''')
+    prompt(messages.get('another_calc'))
     answer = input()
     if answer.lower() == 'y':
         return calculator()
-    return print('See you later!')
-
+    return prompt(messages.get('bye'))
 
 def calculator():
-    prompt('Welcome to Calculator!')
+    prompt(messages.get('welcome'))
 
     # NUM1
-    prompt('Enter a number!')
+    prompt(messages.get('num1'))
     num1 = input()
     while invalid_input(num1):
-        prompt("Error: That isn't a valid number, expecting an integer!")
+        prompt(messages.get('num_error'))
         num1 = input()
     num1 = int(num1)
 
     # NUM2
-    prompt('Enter another number!')
+    prompt(messages.get('num2'))
     num2 = input()
     while invalid_input(num2):
-        prompt("Error: That isn't a valid number, expecting an integer!")
+        prompt(messages.get('num_error'))
         num2 = input()
     num2 = int(num2)
 
     # OPERATION
-    prompt('What operation would you like to perform?:\n'
-            '==> 1) Addition 2) Subtraction 3) Multiplication 4) Division')
+    prompt(messages.get('operation'))
     operation = input()
     while operation not in ['1', '2', '3', '4']:
-        prompt('You must choose 1, 2, 3, or 4.')
+        prompt(messages.get('operation_error'))
         operation = input()
 
     match operation:
@@ -66,9 +78,9 @@ def calculator():
 
     # PRINT RESULTS
     if isinstance(output, str):
-        print(output)
+        prompt(output)
     else:
-        print(f"The result is {output}!")
+        prompt(f"The result is {output}!")
         # ASK USER IF THEY WANT TO CALC AGAIN
         calc_again()
 
