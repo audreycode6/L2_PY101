54a78530cbbcb9c3a5ec802b5a212e299e1c4055
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-08 09:39:48 -0700

    Modified calculator.py & calc_messages.json to translate messages

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -4,13 +4,20 @@
     Perform the calculation and display the results
     Ask user if they wanto continue calculating: 
         Y = start over, N =end program
-    
+    Internationalize the calc_messages.json
     '''
 import json
 with open('calc_messages.json', 'r') as file:
     MESSAGES = json.load(file)
 
-def prompt(message):
+# DEFAULT LANGUAGE FOR MESSAGES: 'es', 'fr', OR 'en'
+LANGUAGE = 'es'
+
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
+
+def prompt(key):
+    message = messages(key, LANGUAGE)
     print(f"==> {message}")
 
 def invalid_input(str_num):
@@ -21,36 +28,36 @@ def invalid_input(str_num):
     return False
 
 def calc_again():
-    prompt(MESSAGES['another_calc'])
+    prompt('another_calc')
     answer = input()
     if answer.lower() == 'y':
         return calculator()
-    return prompt(MESSAGES['bye'])
+    return prompt('bye')
 
 def calculator():
-    prompt(MESSAGES['welcome'])
+    prompt('welcome')
 
     # NUM1
-    prompt(MESSAGES['num1'])
+    prompt('num1')
     num1 = input()
     while invalid_input(num1):
-        prompt(MESSAGES['num_error'])
+        prompt('num_error')
         num1 = input()
     num1 = int(num1)
 
     # NUM2
-    prompt(MESSAGES['num2'])
+    prompt('num2')
     num2 = input()
     while invalid_input(num2):
-        prompt(MESSAGES['num_error'])
+        prompt('num_error')
         num2 = input()
     num2 = int(num2)
 
     # OPERATION
-    prompt(MESSAGES['operation'])
+    prompt('operation')
     operation = input()
     while operation not in ['1', '2', '3', '4']:
-        prompt(MESSAGES['operation_error'])
+        prompt('operation_error')
         operation = input()
 
     match operation:
@@ -62,15 +69,17 @@ def calculator():
             output = num1 * num2
         case '4': # division
             if num2 == 0:
-                output = 'Error: Cannot divide by zero!'
+                output = "Error"
             else:
                 output = num1 / num2
 
     # PRINT RESULTS
     if isinstance(output, str):
-        prompt(output)
+        prompt('zero_error')
     else:
-        prompt(f"The result is {output}!")
+        prompt('result')
+        print(f'    {output}')
+
         # ASK USER IF THEY WANT TO CALC AGAIN
         calc_again()
 
