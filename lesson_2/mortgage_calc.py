fcd3ddd7c868f99a81bbd88de309350bbc9ad99a
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-10 10:08:36 -0700

    Created mortgage_messages.json to clean up code in mortgage_calc.py

--- lesson_2/mortgage_calc.py
+++ lesson_2/mortgage_calc.py
@@ -1,7 +1,11 @@
 ''' build a mortgage calculator that takes
 in loan amount, APR, and loan duration and returns the
 monthly payment assuming that interest is compounded monthly.'''
-# TODO : maybe ask if they want another calc?
+
+import json
+with open('mortgage_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 def prompt(message):
     print(f"==> {message}")
 
@@ -26,70 +30,63 @@ def invalid_loan_duration(loan_duration):
         return True
     return False
 
-# TODO: fix 33, 38, 40 too long
-messages = {
-    "welcome" : "Welcome to Loan Calculator!",
-    "loan_amount" : "What is your loan amount?",
-    "invalid_loan_amount" : "ERROR: Invalid input, enter numbers only. EX: $1234 -> 1234",
-    "APR" : "What is your Annual Percentage Rate (APR)?",
-    "invalid_apr" : "ERROR: Invalid input, enter numbers only. EX: 4% -> 4",
-    "loan_duration" : "Now I'll need your loan duration: years and months.",
-    "years" : "Enter year(s)!",
-    "invalid_year" : "ERROR: Invalid input, enter numbers only. EX: 6 years -> 6",
-    "months" : "Enter month(s)!",
-    "invalid_months" : "ERROR: Invalid input, enter numbers only. EX: 6 months -> 6"
-}
-
 def loan_calc():
-    prompt(messages["welcome"])
+    prompt(MESSAGES["welcome"])
     print()
 
     # LOAN AMOUNT INPUT:
-    prompt(messages["loan_amount"])
+    prompt(MESSAGES["loan_amount"])
     loan_amount_input = input()
     while invalid_loan_amount(loan_amount_input):
-        prompt(messages["invalid_loan_amount"])
+        prompt(MESSAGES["invalid_loan_amount"])
         loan_amount_input = input()
     loan_amount = float(loan_amount_input)
 
     # MONTHLY APR:
-    prompt(messages["APR"])
+    prompt(MESSAGES["APR"])
     annual_percentage_rate_input = input()
     while invalid_apr(annual_percentage_rate_input):
-        prompt(messages["invalid_apr"])
+        prompt(MESSAGES["invalid_apr"])
         annual_percentage_rate_input = input()
     # CONVERT APR TO DECIMAL FORMAT
     annual_percentage_rate = float(annual_percentage_rate_input) * .01
     # APR -> MONTHLY INTEREST RATE
     monthly_interest_rate = annual_percentage_rate / 12
 
-    # LOAN DURATION
-    prompt(messages["loan_duration"])
+    # LOAN DURATION:
+    prompt(MESSAGES["loan_duration"])
     # YEAR(S)
-    prompt(messages["years"])
+    prompt(MESSAGES["years"])
     year_duration = input()
     while invalid_loan_duration(year_duration):
-        prompt(messages["invalid_year"])
+        prompt(MESSAGES["invalid_year"])
         year_duration = input()
     year_month_conversion = int(year_duration) * 12
     # MONTH(S)
-    prompt(messages["months"])
+    prompt(MESSAGES["months"])
     month_duration = input()
     while invalid_loan_duration(month_duration):
-        prompt(messages["invalid_months"])
+        prompt(MESSAGES["invalid_months"])
         month_duration = input()
     month_duration = int(month_duration)
 
     loan_duration_months = year_month_conversion + month_duration
 
-    # MONTHLY PAYMENT :
+    def calc_again():
+        prompt(MESSAGES["another_calculation"])
+        another_calc = input()
+        if another_calc.lower() == 'y':
+            return loan_calc()
+
+        return prompt(MESSAGES["bye"])
+
+    # MONTHLY PAYMENT:
     monthly_payment = loan_amount * (monthly_interest_rate /
     (1 - (1 + monthly_interest_rate) ** (-loan_duration_months)))
-    return f'==> Your monthly payment is: ${monthly_payment:.2f}'
+    prompt(f'Your monthly payment is: ${monthly_payment:.2f}')
+    # ASK USER IF THEY WANT TO CALC AGAIN:
+    print()
+    calc_again()
 
-print(loan_calc())
 
-
-
-
-    
+loan_calc()  
