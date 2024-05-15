21a94cea687f6eb10457841cd4ab9ac4ca1cd234
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-15 16:26:48 -0700

    modified rps_bonus.py; allow user to input abbreviation for their choice + helper functions

--- lesson_2/rps_bonus.py
+++ lesson_2/rps_bonus.py
@@ -10,7 +10,28 @@ add Lizard & Spock to choices:
 
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+VALID_CHOICES = { 
+    'rock' : {'rock', 'r'},
+    'paper' : {'paper', 'p'},
+    'scissors' : {'scissors', 'sc'},
+    'lizard' : {'lizard', 'l'},
+    'spock' : {'spock', 'sp'}
+    }
+
+def choice_invalid(choice):
+    for value in VALID_CHOICES.values():
+        for element in value:
+            if choice != element:
+                continue
+            else:
+                if choice == element:
+                    return False # choice is valid
+
+def update_choice(choice):            
+    for key in VALID_CHOICES.keys():
+        if choice in VALID_CHOICES[key]:
+            update_choice = key
+            return update_choice
 
 def prompt(message):
     print(f"==> {message}")
@@ -23,14 +44,19 @@ def display_welcome():
     print('ROCK * PAPER * SCISSORS * LIZARD * SPOCK')
     print('----------------GO!---------------------')
 
-
+# TODO fix ERROR results not matching rules
+#   CHECK IF THESE REALLY ARE THE ONLY OPTIONS
+#  scissors beats: paper & lizard
+#     paper beats: rock & spock
+#     rock beats: lizard & scissors
+#     lizard beats: Spock & paper
+#     spock beats: scissors & rock
 def display_winner(user, computer):
     if ((user == 'rock' and computer == 'scissors' or computer == 'lizard') or
         (user == 'paper' and computer == 'rock' or computer =='spock') or
         (user == 'scissors' and computer == 'paper' or computer == 'lizard') or
         (user == 'lizard' and computer == 'paper' or computer == 'spock') or
-        (user == 'spock' and computer == 'scissors' or computer == 'rock')
-        ):
+        (user == 'spock' and computer == 'scissors' or computer == 'rock')):
         display_results('YOU WIN! :D')
     elif ((user == 'rock' or user == 'spock' and computer == 'paper') or
         (user == 'paper' or user == 'lizard' and computer == 'scissors') or
@@ -55,11 +81,14 @@ while True: # ROCK PAPER SCISSOR LIZARD SPOCK PROGRAM
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     choice = input().lower()
 
-    while choice not in VALID_CHOICES:
+    while choice_invalid(choice) != False:
         display_error(f'Enter {" OR ".join(VALID_CHOICES)}')
         choice = input().lower()
 
-    computer_choice = random.choice(VALID_CHOICES)
+    # UPDATE CHOICE TO CHOICE THAT MATCHES KEYS FROM VALID_CHOICES: i.e 'r' --> rock
+    choice = update_choice(choice)
+
+    computer_choice = random.choice(list(VALID_CHOICES.keys()))
 
     # DISPLAY CHOICES: USER VS COMPUTER
     prompt(f'You chose {choice} ... computer chose {computer_choice}')
