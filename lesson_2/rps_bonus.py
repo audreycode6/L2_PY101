2674aec559ecedb7d1e250486fa0a8504c561a3d
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-16 09:44:48 -0700

    modified rps_bonus.py;  modified display_winner() to clean up code from many if + and/or statements ; updated other functions update_choice() abd choice_invalid() to clean up code

--- lesson_2/rps_bonus.py
+++ lesson_2/rps_bonus.py
@@ -7,10 +7,13 @@ add Lizard & Spock to choices:
     lizard beats: Spock & paper
     spock beats: scissors & rock
 '''
-
+# TODO pylint upset about things that are needed:
+    # upset with redefing name ' choice' and 'update_choice' from outer scope 
+        # necessary to update the users input choice to match VALID_CHOICES key
+    # consider iterating with .items() and .keys() -- doesnt work tho
 import random
 
-VALID_CHOICES = { 
+VALID_CHOICES = {
     'rock' : {'rock', 'r'},
     'paper' : {'paper', 'p'},
     'scissors' : {'scissors', 'sc'},
@@ -19,19 +22,20 @@ VALID_CHOICES = {
     }
 
 def choice_invalid(choice):
-    for value in VALID_CHOICES.values():
-        for element in value:
-            if choice != element:
-                continue
-            else:
-                if choice == element:
-                    return False # choice is valid
+    choice_is_invalid = True
 
-def update_choice(choice):            
+    for choices in VALID_CHOICES.values():
+        if choice in choices:
+            choice_is_invalid = False
+
+    return choice_is_invalid
+
+def update_choice(choice):
     for key in VALID_CHOICES.keys():
         if choice in VALID_CHOICES[key]:
-            update_choice = key
-            return update_choice
+            update_choice_full_name = key
+
+    return update_choice_full_name
 
 def prompt(message):
     print(f"==> {message}")
@@ -44,25 +48,17 @@ def display_welcome():
     print('ROCK * PAPER * SCISSORS * LIZARD * SPOCK')
     print('----------------GO!---------------------')
 
-# TODO fix ERROR results not matching rules
-#   CHECK IF THESE REALLY ARE THE ONLY OPTIONS
-#  scissors beats: paper & lizard
-#     paper beats: rock & spock
-#     rock beats: lizard & scissors
-#     lizard beats: Spock & paper
-#     spock beats: scissors & rock
 def display_winner(user, computer):
-    if ((user == 'rock' and computer == 'scissors' or computer == 'lizard') or
-        (user == 'paper' and computer == 'rock' or computer =='spock') or
-        (user == 'scissors' and computer == 'paper' or computer == 'lizard') or
-        (user == 'lizard' and computer == 'paper' or computer == 'spock') or
-        (user == 'spock' and computer == 'scissors' or computer == 'rock')):
+    winning_combinations = {
+        'rock' : ['scissors', 'lizard'],
+        'paper' : ['rock', 'spock'],
+        'scissors' : ['paper', 'lizard'],
+        'lizard' : ['spock', 'paper'],
+        'spock' : ['scissors', 'rock']
+        }
+    if computer in winning_combinations.get(user, []):
         display_results('YOU WIN! :D')
-    elif ((user == 'rock' or user == 'spock' and computer == 'paper') or
-        (user == 'paper' or user == 'lizard' and computer == 'scissors') or
-        (user == 'scissors' or user == 'lizard' and computer == 'rock') or
-        (user == 'paper' or user == 'spock' and computer == 'lizard') or
-        (user == 'scissors' or user == 'rock' and computer == 'spock')):
+    elif user in winning_combinations.get(computer, []):
         display_results('COMPUTER WINS!')
     else:
         display_results("IT'S A TIE!")
@@ -81,19 +77,20 @@ while True: # ROCK PAPER SCISSOR LIZARD SPOCK PROGRAM
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     choice = input().lower()
 
-    while choice_invalid(choice) != False:
+    while choice_invalid(choice) is not False:
         display_error(f'Enter {" OR ".join(VALID_CHOICES)}')
         choice = input().lower()
 
-    # UPDATE CHOICE TO CHOICE THAT MATCHES KEYS FROM VALID_CHOICES: i.e 'r' --> rock
-    choice = update_choice(choice)
+    # UPDATE USERS CHOICE TO MATCHING CHOICE FROM KEYS IN VALID_CHOICES:
+        # i.e 'r' --> rock
+    user_choice = update_choice(choice)
 
     computer_choice = random.choice(list(VALID_CHOICES.keys()))
 
     # DISPLAY CHOICES: USER VS COMPUTER
-    prompt(f'You chose {choice} ... computer chose {computer_choice}')
+    prompt(f'You chose {user_choice} ... computer chose {computer_choice}')
 
-    display_winner(choice, computer_choice)
+    display_winner(user_choice, computer_choice)
 
     # CHECK IF USER WANTS TO PLAY AGAIN
     print('\n==> Play again? (y/n)')
