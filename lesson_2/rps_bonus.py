28404e0801c0bf6bc302dc60ab2c3e1a49681183
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-21 15:00:12 -0700

    modified rps_bonus.py; cleaned up code: clear screen during transitions, clearer rules, format messages, use helper functions for main program

--- lesson_2/rps_bonus.py
+++ lesson_2/rps_bonus.py
@@ -10,6 +10,8 @@ added Lizard & Spock to choices & allow abbreviations for choice input.
 '''
 
 import random
+import os
+from time import sleep
 
 VALID_CHOICES = {
     'rock' : {'rock', 'r'},
@@ -19,20 +21,9 @@ VALID_CHOICES = {
     'spock' : {'spock', 'sp'}
     }
 
-def choice_invalid(user_choice):
-    choice_is_invalid = True
-    for choices in VALID_CHOICES.values():
-        if user_choice in choices:
-            choice_is_invalid = False
-
-    return choice_is_invalid
-
-def update_choice(user_choice):
-    for key, section in VALID_CHOICES.items():
-        if user_choice in section:
-            update_choice_full_name = key
-
-    return update_choice_full_name
+def clear_screen(seconds):
+    sleep(seconds)
+    os.system('clear')
 
 def prompt(message):
     print(f"==> {message}")
@@ -42,19 +33,48 @@ def display_error(message):
 
 def display_game_rules():
     print('--------------------------------------------------')
-    print('Welcome to Rock, Paper, Scissors, Lizard, Spock!\n')
-    print('                 RULES:')
-    print('''        scissors beats: paper & lizard
-        paper beats: rock & spock
-        rock beats: lizard & scissors
-        lizard beats: Spock & paper
-        spock beats: scissors & rock''')
-    print('--------------------------------------------------\n')
+    print(' Welcome to Rock, Paper, Scissors, Lizard, Spock!\n')
+    print('                 * RULES *')
+    print('''You have 5 rounds to try to beat your oponent.
+Choose between 5 objects:
+    * SCISSORS beats paper & lizard
+    * PAPER beats rock & Spock
+    * ROCK beats lizard & scissors
+    * LIZARD beats Spock & paper
+    * SPOCK beats scissors & rock''')
+    print('--------------------------------------------------')
+    prompt('Enter any key to start')
+    input()
 
 def display_welcome(game_round):
-    print(f'\n------------- ROUND {game_round} of 5 ---------------')
+    print(f'------------- ROUND {game_round} of 5 ---------------')
     print('ROCK * PAPER * SCISSORS * LIZARD * SPOCK')
     print('---------------- GO! ---------------------')
+
+def choice_invalid(user_choice):
+    choice_is_invalid = True
+    for choices in VALID_CHOICES.values():
+        if user_choice in choices:
+            choice_is_invalid = False
+    return choice_is_invalid
+
+def get_choice():
+    prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
+    choice = input().lower()
+
+    while choice_invalid(choice) is True:
+        display_error(f'''Enter {" OR ".join(VALID_CHOICES)}.
+        Abbreviations also accepted: r, p, sc, l, sp''')
+        choice = input().lower()
+    return choice
+
+# Update users choice to matching choice from keys in VALID_CHOICES:
+    # i.e 'r' --> rock
+def update_choice(user_choice):
+    for key, section in VALID_CHOICES.items():
+        if user_choice in section:
+            update_choice_full_name = key
+    return update_choice_full_name
 
 def display_winner(user, computer, game_round):
     winning_combinations = {
@@ -66,29 +86,41 @@ def display_winner(user, computer, game_round):
         }
 
     if computer in winning_combinations.get(user, []):
-        display_results(f'ROUND {game_round}: You win!')
+        display_results(f'ROUND {game_round}: YOU WIN!')
         return "user_wins"
     if user in winning_combinations.get(computer, []):
-        display_results(f'ROUND {game_round}: Computer wins!')
+        display_results(f'ROUND {game_round}: COMPUTER WINS!')
         return "computer_wins"
 
-    display_results(f"ROUND {game_round}: Tie!")
+    display_results(f"ROUND {game_round}: TIE!")
     return "tie"
 
 def display_results(message):
-    print(f'    {message}')
+    print(f'    ** {message} **')
+
+def update_counters(score,
+                    user_win_count,
+                    computer_win_count,
+                    tie_count):
+    if score == 'user_wins':
+        user_win_count += 1
+    elif score == 'computer_wins':
+        computer_win_count += 1
+    else:
+        tie_count += 1
 
 def display_final_winner(user_wins, computer_wins, ties):
-    print("\n-------------- GAME OVER -----------------")
+    print("-------------- GAME OVER -----------------")
     if user_wins > computer_wins:
         print('               YOU WIN! :D')
     elif user_wins < computer_wins:
         print('             COMPUTER WINS!')
     else:
         print('           THERES BEEN A TIE! :O')
-    print(f'                 wins: {user_wins}')
-    print(f'               losses: {computer_wins}')
-    print(f'                  tie: {ties}')
+    print('\n              your results')
+    print(f'                 win: {user_wins}')
+    print(f'                loss: {computer_wins}')
+    print(f'                 tie: {ties}')
     print('------------------------------------------')
 
 def prompt_play_again():
@@ -102,58 +134,56 @@ def prompt_play_again():
 
     if answer[0] == 'n':
         return display_bye()
+
+    clear_screen(0.5)
     return rock_paper_scissors_l_sp()
 
 def display_bye():
-    prompt('Thanks for playing, see you later!')
-    print('-----------------------------------------')
+    prompt('''Thanks for playing Rock, Paper, Scissors, Lizard, Spock!
+    See you later!''')
+    clear_screen(3)
 
 # ROCK PAPER SCISSOR LIZARD SPOCK PROGRAM - main entry point
 def rock_paper_scissors_l_sp():
-    game_round_counter = 0
-    computer_wins_counter = 0
-    user_wins_counter = 0
-    tie_counter = 0
+    display_game_rules()
+    clear_screen(.5)
 
-    while game_round_counter < 5:
-        game_round_counter += 1
-        display_welcome(game_round_counter)
+    game_round_count = 0
+    computer_win_count = 0
+    user_win_count = 0
+    tie_count = 0
 
-        # ASK USER FOR THEIR CHOICE: R P SC L SP
-        prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
-        choice = input().lower()
+    while game_round_count < 5:
+        game_round_count += 1
+        display_welcome(game_round_count)
 
-        while choice_invalid(choice) is not False:
-            display_error(f'''Enter {" OR ".join(VALID_CHOICES)}.
-            Abbreviations also accepted: r, p, sc, l, sp''')
-            choice = input().lower()
-
-        # UPDATE USERS CHOICE TO MATCHING CHOICE FROM KEYS IN VALID_CHOICES:
-            # i.e 'r' --> rock
-        user_fullname_choice = update_choice(choice)
-
+        # GET PLAYER CHOICES:
+        user_choice = get_choice()
+        user_fullname_choice = update_choice(user_choice)
         computer_choice = random.choice(list(VALID_CHOICES.keys()))
 
-        # DISPLAY CHOICES: USER VS COMPUTER
-        prompt(
-        f"You chose {user_fullname_choice}. Computer chose {computer_choice}."
-        )
+        # ROUND RESULTS
+        display_results(f'''You chose {user_fullname_choice}. **
+    ** Computer chose {computer_choice}.''')
 
+        # UPDATE COUNTERS
         score = display_winner(
             user_fullname_choice,
             computer_choice,
-            game_round_counter
+            game_round_count
             )
         if score == 'user_wins':
-            user_wins_counter += 1
+            user_win_count += 1
         elif score == 'computer_wins':
-            computer_wins_counter += 1
+            computer_win_count += 1
         else:
-            tie_counter += 1
+            tie_count += 1
+        clear_screen(3)
 
     # FINAL RESULTS
-    display_final_winner(user_wins_counter, computer_wins_counter, tie_counter)
-
+    display_final_winner(user_win_count, computer_win_count, tie_count)
+    sleep(2)
     prompt_play_again()
-display_game_rules()
+
+# RUN PROGRAM
 rock_paper_scissors_l_sp()
